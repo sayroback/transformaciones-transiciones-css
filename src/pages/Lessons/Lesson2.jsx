9f79666dd630af7c1e936eea312adf4a3deb6ee8
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-07-22 22:04:51 -0500

    Pseudo-elementos

--- src/pages/Lessons/Lesson2.jsx
+++ src/pages/Lessons/Lesson2.jsx
@@ -9,41 +9,23 @@ export const Lesson2 = () => {
         <h1>L{`Lesson ${numLesson}:`} Pseudo-elementos</h1>
         <h2>Ejercicio</h2>
         <div className={`lesson${numLesson}__exercise`}>
-          <a href="/">Sayroback</a>
-          <a href="/hola">Sayroback</a>
-          <p className="yes">Si</p>
-          <p className="not">No</p>
-          <div className="container">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
+          <div>Hola</div>
         </div>
         <div className={`lesson${numLesson}__notes`}>
           <h1>Notas</h1>
-          <h3>:link</h3>
+          <h3>::before</h3>
           <p>
-            Selecciona aquellos enlaces que <b>aun no han</b> sido visitados.
+            Crea un pseudoelemento que es el primer hijo del elemento
+            seleccionado. Es usado normalmente para añadir contenido estético a
+            un elemento, usando la propiedad content. Este elemento se muestra
+            en línea con el texto de forma predeterminada.
           </p>
-          <h3>:visited</h3>
+          <h3>::after</h3>
           <p>
-            Selecciona aquellos enlaces que <b>ya</b> han sido visitados.
-          </p>
-          <h3>:hover</h3>
-          <p>
-            Generalmente se activa cuando el usuario se desplaza sobre un
-            elemento con el cursor
-          </p>
-          <h3>:not()</h3>
-          <p>
-            La pseudoclase :not() representa elementos que no coinciden con una
-            lista de selectores. Como evita que se seleccionen elementos
-            específicos, se lo conoce como la pseudoclase de negación.
-          </p>
-          <h3>:nth-child(A)</h3>
-          <p>
-            Toma al elemento según la posición en la que este. Comienza del 1
-            hasta n.
+            Crea un pseudo-elemento que es el último hijo del elemento
+            seleccionado. Es comunmente usado para añadir contenido cosmético a
+            un elemento con la propiedad content.Es en linea (inline) de forma
+            predeterminada.
           </p>
         </div>
       </div>
