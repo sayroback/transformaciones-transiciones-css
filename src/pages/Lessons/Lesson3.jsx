3d9a9b9538c1009881de91e9f593d2c6130e8a7b
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-07-22 22:49:25 -0500

    Transform translate

--- src/pages/Lessons/Lesson3.jsx
+++ src/pages/Lessons/Lesson3.jsx
@@ -21,13 +21,62 @@ export const Lesson3 = () => {
             mediante funciones. Estas funciones permitirán trasladar, escalar,
             rotar o torcer a lo largo, ancho y profundidad del elemento.
           </p>
-          <h3>::after</h3>
+          <h3>Translate</h3>
           <p>
-            Crea un pseudo-elemento que es el último hijo del elemento
-            seleccionado. Es comunmente usado para añadir contenido cosmético a
-            un elemento con la propiedad content.Es en linea (inline) de forma
-            predeterminada.
+            Es una función de la propiedad “transform” que te permite trasladar
+            un elemento HTML a través de los ejes del navegador. El valor que
+            recibe puede ser una longitud (px, rem, etc.) o un porcentaje.
           </p>
+          <p>
+            <a href="https://giphy.com/gifs/KRRSOKFINjskbRNENr">via GIPHY</a>
+          </p>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <strong>Tipo</strong>
+                </th>
+                <th>
+                  <strong>Valor con los argumentos que recibe</strong>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Eje X y Y</td>
+                <td>translate(x,y)</td>
+              </tr>
+              <tr>
+                <td>Eje X</td>
+                <td>translateX(x)</td>
+              </tr>
+              <tr>
+                <td>Eje Y</td>
+                <td>translateY(y)</td>
+              </tr>
+              <tr>
+                <td>Eje Z</td>
+                <td>translateZ(z)</td>
+              </tr>
+              <tr>
+                <td>Múltiple</td>
+                <td>translate3d(x,y,z)</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            Además de translate, existen otras funciones que permiten
+            transformar el elemento HTML para iniciar una animación.
+          </p>
+          <iframe
+            title="This is a unique title"
+            src="https://giphy.com/embed/KRRSOKFINjskbRNENr"
+            width="480"
+            height="270"
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+          />
         </div>
       </div>
     </LayoutLessons>
