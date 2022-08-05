895a4c7e73f3a8251f01491d2460e3e7001bcfaf
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-08-05 15:09:47 -0500

    GAME: bunny ears

---
+++ src/pages/Game/Bony.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Bony = () => {
+  return (
+    <>
+      <div className="left-ear--outer"></div>
+      <div className="left-ear--inner"></div>
+      <div className="right-ear--outer"></div>
+      <div className="right-ear--inner"></div>
+      <div className="head">
+        <div className="head__eye head__eye--left"></div>
+        <div className="head__eye head__eye--right"></div>
+      </div>
+    </>
+  );
+};
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "../../styles/game/game.scss";
+import { Bony } from "./Bony";
 import { Counter } from "./Counter";
 
 export const Game = () => {
   return (
     <div className="game">
       <div className="phone">
-        <div className="layer-1"></div>
-        <div className="layer-2"></div>
+        <div className="layer-1">
+          <Bony />
+        </div>
+        <div className="layer-2">
+          <Bony />
+        </div>
         <div className="layer-3"></div>
-        <div className="layer-4"></div>
+        <div className="layer-4">
+          <Bony />
+        </div>
         <div className="layer-5"></div>
-        <div className="layer-6"></div>
+        <div className="layer-6">
+          <Bony />
+        </div>
         <div className="layer-7"></div>
         <div className="layer-8"></div>
         <div className="layer-9"></div>
