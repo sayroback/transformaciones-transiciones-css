884cd7f8282331e4927868ab5fa7029a458b4303
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-08-05 12:53:16 -0500

    GAME: Counter

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "../../styles/game/game.scss";
+import { Counter } from "./Counter";
 
 export const Game = () => {
-  return <div>Hola soy un Juego</div>;
+  return (
+    <div className="game">
+      <div className="phone">
+        <div className="layer-1"></div>
+        <div className="layer-2"></div>
+        <div className="layer-3"></div>
+        <div className="layer-4"></div>
+        <div className="layer-5"></div>
+        <div className="layer-6"></div>
+        <div className="layer-7"></div>
+        <div className="layer-8"></div>
+        <div className="layer-9"></div>
+        <div className="layer-10"></div>
+      </div>
+      <Counter />
+    </div>
+  );
 };
