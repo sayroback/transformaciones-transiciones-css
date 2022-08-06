b8d18f3985c03a752aa8745d5b2355be918569be
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-08-06 14:17:09 -0500

    GAME: score

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,32 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 import "../../styles/game/game.scss";
 import { Bonny } from "./Bonny";
 import { Cloud } from "./Cloud";
-import { Counter } from "./Counter";
 import { Tree } from "./Tree";
 import { Wall } from "./Wall";
 
+const Layer = styled.div`
+  animation-name: layer;
+  animation-duration: ${(props) => props.speed}s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+
+  @keyframes layer {
+    0% {
+      bottom: 0;
+    }
+    25% {
+      bottom: -80px;
+    }
+    100% {
+      bottom: 0;
+    }
+  }
+`;
+
 export const Game = () => {
+  const [score, setScore] = React.useState(0);
+  const [speed, setSpeed] = React.useState(3.5);
+
+  const handleClick = () => {
+    let newScore = score + 1;
+    setScore(newScore);
+    let newSpeed = -0.075 * newScore + 3.5;
+    setSpeed(newSpeed);
+  };
+  console.log(speed);
+  console.log(score);
+
+  const bonnys = [1, 2, 4, 6];
+
   return (
     <div className="game">
       <div className="phone">
-        <div className="layer-1">
-          <Bonny />
+        <div className="Title">
+          <h1>Hit a rabbit</h1>
+          <h3 className="score">Score: {score}</h3>
         </div>
-        <div className="layer-2">
-          <Bonny />
-        </div>
+        {bonnys.map((bonny) => (
+          <Layer
+            speed={speed}
+            className={`layer-${bonny}`}
+            onClick={handleClick}
+          >
+            <Bonny />
+          </Layer>
+        ))}
         <div className="layer-3">
           <Wall />
         </div>
-        <div className="layer-4">
-          <Bonny />
-        </div>
         <div className="layer-5">
           <Wall />
-        </div>
-        <div className="layer-6">
-          <Bonny />
         </div>
         <div className="layer-7">
           <Wall />
@@ -41,7 +75,6 @@ export const Game = () => {
           <Cloud />
         </div>
       </div>
-      <Counter />
     </div>
   );
 };
